8f4160704066f3786634e1d662a6ca305b79d8dd
BATTAGEL <matthew.battagel@hpe.com>
2022-01-26 16:11:25 +0000

    Front end

--- api/api.py
+++ api/api.py
@@ -1,10 +1,8 @@
-# https://blog.miguelgrinberg.com/post/how-to-create-a-react--flask-project
-
 import time
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/time')
+@app.route('/api/time')
 def get_current_time():
     return {'time': time.time()}
---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    console.log(fetch('/api/time').then(rez => {
+      if (!rez.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return rez.json();
+    }).then(data => {
+      if (!data.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return setCurrentTime(data);
+    }))
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <p>The current time is {currentTime}.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
