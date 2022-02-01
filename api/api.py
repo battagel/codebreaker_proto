91a6fa1467baca7eec354007b06adc28e28e906c
BATTAGEL <matthew.battagel@hpe.com>
2022-02-01 19:53:28 +0000

    Added home page

--- api/api.py
+++ api/api.py
@@ -1,8 +1,83 @@
 import time
 from flask import Flask
+import random
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../build', static_url_path='/')
 
-@app.route('/api/time')
-def get_current_time():
-    return {'time': time.time()}
+num_colours = 6
+
+@app.errorhandler(404)
+def not_found(e):
+    return app.send_static_file('index.html')
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+
+@app.route('/api/home')
+def home():
+    code = gen_random_code()
+    colour_code = code_to_colour(code)
+
+    return {"code": code, "colour_code": colour_code}
+
+def gen_random_code(length=5):
+    """ 
+        Generates a random 5 number combination for later use
+
+        RETURNS - list of values from length  
+    """
+
+    code = []
+
+    for _ in range(0,length):
+        code.append(random.randint(1,num_colours))
+    
+    print(f"Generated code {code}")
+    return code
+
+def code_to_colour(code):
+    """
+        Changes an integer list into a colour list
+    """
+    colour_map = {
+        1: "#ffffff",
+        2: "#000000",
+        3: "#FF0000",
+        4: "#00FF00",
+        5: "#0000FF",
+        6: "#FFFF00",
+        7: "#FF00FF",
+        8: "#00FFFF"
+    }
+
+    colour_code = []
+    for digit in code:
+        colour_code.append(colour_map[digit])
+
+    print(f"Converted code:{code} to {colour_code}")
+    return colour_code
+
+def check_guess(guess, code):
+    """
+    Checks the guess of the user against the correct code
+
+    RETURNS: Tuple with number of blacks, number of whites
+    """
+
+    num_blacks = 0
+    num_whites = 0
+
+    for pos in range(0,len(code)):
+        if guess[pos] == code[pos]:
+            num_blacks += 1
+            guess[pos], code[pos] = "B" "B"
+    for posA in range(0, len(code)):
+        for posB in range(0, len(code)):
+            if code[posA] == guess[posB] and code[posA] != "B" and code[posA] != "W":
+                num_whites += 1
+                pos_in_code = code.index(guess[posB])
+                guess[posB], code[pos_in_code] = "W", "W"
+    
+    return num_blacks, num_whites
--- src/App.js
+++ src/App.js
@@ -1,41 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
+import React from 'react';
 import './App.css';
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
-  const [currentTime, setCurrentTime] = useState(0);
-
-  useEffect(() => {
-    console.log(fetch('/api/time').then(rez => {
-      if (!rez.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return rez.json();
-    }).then(data => {
-      if (!data.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return setCurrentTime(data);
-    }))
-  }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>The current time is {currentTime}.</p>
-      </header>
+		<BrowserRouter>
+			<div className="App-header">
+				<div className="App-nav middle">
+					<Link className="App-link" to="/">Home</Link>
+					&nbsp;|&nbsp;
+					<Link className="App-link" to="/about">About</Link>
+				</div>
+			</div>
+			<div className="App-body">
+				<Routes>
+					<Route path="/" element={<Home/>}/>
+					<Route path="/about" element={<About/>}/>
+					<Route component={Error}/>
+				</Routes>
+			</div>
+	  	</BrowserRouter>
     </div>
   );
 }
